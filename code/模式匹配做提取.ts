2b39af920031f605465476564ead6058cf847a08
gf <1454556135@qq.com>
2022-02-23 18:21:15 +0800

    [feat]new code

--- code/模式匹配做提取.ts
+++ code/模式匹配做提取.ts
@@ -2,15 +2,26 @@
 type p = Promise<'guangFa'>;
 type GetValueType<P> = P extends Promise<infer Value> ? Value : never;
 type GetValueResult = GetValueType<p>
-// 数组类型想提取第一个元素的类型
+
+/* 数组 */
+
+// 数组提取第一个元素的类型
 type arr = [1,2,3];
 type GetFirst<Arr extends unknown[]> = Arr extends [infer First, ...unknown[]] ? First : never;
 type GetFirstResult = GetFirst<arr>
-// 数组类型想提取最后一个元素的类型
-type arr1 = [1,2,3];
+// 数组提取最后一个元素的类型
 type GetLast<Arr extends unknown[]> = Arr extends [...unknown[], infer Last] ? Last : never;
-type GetLastResult = GetFirst<arr>
+type GetLastResult = GetFirst<[1,2,3]>
 // 取数组剩下的
-type arr2=[1,2,3];
 type GetMore<Arr extends unknown[]> = Arr extends [...infer Rest, unknown] ? Rest : never;
-type GetMoreResult = GetMore<arr2>
+type GetMoreResult = GetMore<[1,2,3]>
+
+/* 字符串 */
+
+// 判断字符串是否以某个前缀开头
+type StartWith<Str extends string, Prefix extends string> = Str extends `${Prefix}${string}` ? true : false;
+type startWithResult = StartWith<'guanga fa','guang'>
+// 判断字符串是否以某个字符串结尾
+type EndWith<Str extends string, EndFix extends string> = Str extends `${string}${EndFix}` ? true : false;
+type EndWithResult = EndWith<'guang fa','fa'>
+// 字符串的替换
