2e645e20301b287632f8ecf6aeca245f904a8fdb
hgf <1454556135@qq.com>
2022-02-27 00:05:40 +0800

    移除变量

--- code/递归复用做循环.ts
+++ code/递归复用做循环.ts
@@ -1,25 +1,28 @@
 // ts没有循环的语法、但是支持递归、所以我们需要用递归去代替循环的操作
 
+/****************** Promise *******************************************************/
+
 // 【案例1】：提取不确定层数的promise的结果
-type p = Promise<Promise<Promise<1>>>
+type p1 = Promise<Promise<Promise<1>>>
 type DeepPromise<P extends Promise<unknown>> = 
     P extends Promise<infer ValueType>
         ? ValueType extends Promise<unknown>
             ? DeepPromise<ValueType>
             : ValueType
         : never;
-type getDeepPromise = DeepPromise<p>;
+type getDeepPromise = DeepPromise<p1>;
 
+/****************** 数组 *******************************************************/
 
-// 【案例2】：数组反转
-type arr = [1,2,3]
+// 【案例1】：数组反转
+type arr1 = [1,2,3]
 type ReverseArr<Arr extends unknown[]> = 
     Arr extends [ infer First, ...infer Rest]
         ? [...ReverseArr<Rest>, First]
         : Arr;
-type GetReverseArrRes = ReverseArr<arr>
+type GetReverseArrRes = ReverseArr<arr1>
 
-// 【案例3】：Includes
+// 【案例2】：Includes
    // 查找
 type Includes<Arr extends unknown[], FineItem> = 
     Arr extends [infer First, ...infer Rest]
@@ -27,7 +30,7 @@ type Includes<Arr extends unknown[], FineItem> =
             ? true
             : Includes<Rest, FineItem>
         : false;
-
+// 【案例3】：Includes
     // 删除
 type Removes<Arr extends unknown[], Item> = 
     Arr extends [infer First, ...infer Rest]
@@ -35,6 +38,7 @@ type Removes<Arr extends unknown[], Item> =
             ? Removes<Rest, Item>
             : [First, ...Removes<Rest, Item>]
         : Arr    
+// 【案例4】：Includes
     // 指定生成长度为n的数组并把value填充进去
 type BuildArray<
     Length extends number, 
