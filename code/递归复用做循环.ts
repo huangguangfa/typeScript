4f4a93b434b948607390f75547a74dcecb2d258e
hgf <1454556135@qq.com>
2022-02-27 00:00:53 +0800

    [feat]数组操作递归

--- code/递归复用做循环.ts
+++ code/递归复用做循环.ts
@@ -14,7 +14,30 @@ type getDeepPromise = DeepPromise<p>;
 // 【案例2】：数组反转
 type arr = [1,2,3]
 type ReverseArr<Arr extends unknown[]> = 
-    Arr extends [ infer First , ...infer Rest]
+    Arr extends [ infer First, ...infer Rest]
         ? [...ReverseArr<Rest>, First]
         : Arr;
 type GetReverseArrRes = ReverseArr<arr>
+
+// 【案例3】：Includes
+   // 查找
+type Includes<Arr extends unknown[], FineItem> = 
+    Arr extends [infer First, ...infer Rest]
+        ? First extends FineItem
+            ? true
+            : Includes<Rest, FineItem>
+        : false;
+
+    // 删除
+type Removes<Arr extends unknown[], Item> = 
+    Arr extends [infer First, ...infer Rest]
+        ? First extends Item 
+            ? Removes<Rest, Item>
+            : [First, ...Removes<Rest, Item>]
+        : Arr    
+    // 指定生成长度为n的数组并把value填充进去
+type BuildArray<
+    Length extends number, 
+    Item = unknown,
+    Arr extends unknown[] = []> =  Arr["length"] extends Length ? Arr : BuildArray<Length, Item, [...Arr, Item]>
+
